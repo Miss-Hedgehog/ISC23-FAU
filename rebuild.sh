967961aec8c7bde0a01003a1aa0c12439c162a67
gcujrrly <77671303+Miss-Hedgehog@users.noreply.github.com>
2023-03-25 23:11:00 +0800

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -11,10 +11,17 @@
 #  the same compiler being used here and is loaded in the run-time
 #  environment (e.g. LD_LIBRARY_PATH).
 #################################################################
-
+MPI=intelmpi/2021.7.0
+if [[ "$MPI" =~ ^intel ]]; then
+        module load hdf5/1.12.2-intel2021.7.0-impi
+        export I_MPI_F90=ifort
+else
+        module load  hdf5/1.12.2-intel2021.7.0-ompi
+        export OMPI_MPIF90=ifort
+fi
 # Location of local hdf5 installed with same compiler being used for POT3D:
-HDF5_INCLUDE_DIR="/opt/psi/intel/ext_deps/deps/hdf5/include"
-HDF5_LIB_DIR="/opt/psi/intel/ext_deps/deps/hdf5/lib"
+HDF5_INCLUDE_DIR="/apps/SPACK/0.19.0/opt/linux-almalinux8-icelake/intel-2021.7.0/hdf5-1.12.2-l7wmsy76ansuprtkjfj6nsvfbd2owmkc/include"
+HDF5_LIB_DIR="/apps/SPACK/0.19.0/opt/linux-almalinux8-icelake/intel-2021.7.0/hdf5-1.12.2-l7wmsy76ansuprtkjfj6nsvfbd2owmkc/lib"
 # Fortran HDF5 library flags (these can be version dependent):
 HDF5_LIB_FLAGS="-lhdf5_fortran -lhdf5hl_fortran -lhdf5 -lhdf5_hl"
 
