afeeee27f1c1cd07fcb28ff9bc2663b1a436da3b
gcujrrly <77671303+Miss-Hedgehog@users.noreply.github.com>
2023-03-25 23:41:29 +0800

    Create test_small_run.sh

---
+++ test_small_run.sh
@@ -0,0 +1,21 @@
+NP=16
+
+cd testsuite
+
+POT3D_HOME=$PWD/..
+TEST="small"
+
+cp ${POT3D_HOME}/testsuite/${TEST}/input/* ${POT3D_HOME}/testsuite/${TEST}/run/
+cd ${POT3D_HOME}/testsuite/${TEST}/run
+
+echo "Running POT3D with $NP MPI rank..."
+mpirun -np $NP ${POT3D_HOME}/bin/pot3d > pot3d.log
+echo "Done!"
+
+# Get runtime:
+runtime=($(tail -n 5 timing.out | head -n 1))
+echo "Wall clock time: ${runtime[6]} seconds"
+echo " "
+
+# Validate run:
+${POT3D_HOME}/scripts/pot3d_validation.sh pot3d.out ${POT3D_HOME}/testsuite/${TEST}/validation/pot3d.out
