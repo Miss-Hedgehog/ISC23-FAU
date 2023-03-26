071bb0016a586605a86232dc307635c4015b35bd
gcujrrly <77671303+Miss-Hedgehog@users.noreply.github.com>
2023-03-26 20:41:07 +0800

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -27,7 +27,7 @@ HDF5_LIB_FLAGS="-lhdf5_fortran -lhdf5_hl_fortran -lhdf5 -lhdf5_hl"
 # Please set the compile flags based on your compiler and hardware setup.
 ###########################################################################
 
-FFLAGS="-O3"
+FFLAGS="-O3 -xHost -assume byterecl -heap-arrays"
 
 ###########################################################################
 # If using NV HPC SDK for GPUs, with CUDA version >= 11.3, you can set 
