1f3a9770a4a7c28f57e9e049efdcf371aff853a0
gcujrrly <77671303+Miss-Hedgehog@users.noreply.github.com>
2023-04-17 12:45:48 +0800

    Update profile.sh

--- profile.sh
+++ profile.sh
@@ -6,7 +6,7 @@
 #SBATCH --output=%j.out              
 #SBATCH --error=%j.err   
 
-source /home/hpc/b154dc/b154dc19/intel/oneapi/vtune/latest/apsvars.sh
+source  /home/hpc/b154dc/b154dc19/intel/oneapi/vtune/latest/vtune-vars.sh
 #module load intel/2021.7.0
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/apps/SPACK/0.19.0/opt/linux-almalinux8-icelake/intel-2021.7.0/hdf5-1.12.2-l7wmsy76ansuprtkjfj6nsvfbd2owmkc/lib
 
@@ -17,6 +17,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/apps/SPACK/0.19.0/opt/linux-almalinux8-
 #export IPM_STATS=all
 #export IPM_LOGWRITER=serial
 #export LD_PRELOAD=$IPM_DIR/lib/libipm.so
+export I_MPI_STATS=all
 cd testsuite
 
 POT3D_HOME=$PWD/..
@@ -26,7 +27,7 @@ cp ${POT3D_HOME}/testsuite/${TEST}/input/* ${POT3D_HOME}/testsuite/${TEST}/run/
 cd ${POT3D_HOME}/testsuite/${TEST}/run
 
 echo "Running POT3D with $NP MPI rank..."
-mpirun -np 288 -ppn 72 aps ${POT3D_HOME}/bin/pot3d_impi > pot3d.log
+mpirun -np 288 -ppn 72  ${POT3D_HOME}/bin/pot3d_impi > pot3d.log
 echo "Done!"
 
 # Get runtime:
