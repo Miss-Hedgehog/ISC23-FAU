62d55742419d2b0ca49c587f659ee6cada01bd28
gcujrrly <77671303+Miss-Hedgehog@users.noreply.github.com>
2023-04-27 09:14:40 +0800

    Update rebuild2.sh

--- rebuild2.sh
+++ rebuild2.sh
@@ -25,7 +25,7 @@ HDF5_LIB_FLAGS="-lhdf5_fortran -lhdf5_hl_fortran -lhdf5 -lhdf5_hl"
 # Please set the compile flags based on your compiler and hardware setup.
 ###########################################################################
 
-FFLAGS="-O3 -stdpar-multicore -mp"
+FFLAGS="-O3  -stdpar-multicore -acc-multicore -march=native -mp"
 
 ###########################################################################
 # If using NV HPC SDK for GPUs, with CUDA version >= 11.3, you can set 
@@ -36,7 +36,7 @@ FFLAGS="-O3 -stdpar-multicore -mp"
 ###########################################################################
 
 POT3D_CUSPARSE=0
-CCFLAGS="-O3"
+CCFLAGS="-O3 -stdpar-multicore -acc-multicore -mp -march=native"
 
 ###########################################################################
 ###########################################################################
